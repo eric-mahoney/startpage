01f36d4fd2c32c057c58155dfe01b5c8735b0be8
Eric Mahoney <eric.mahoney22@gmail.com>
2021-11-03 22:15:13 -0400

    added theme provider to the index

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from "@mui/material";
 import React from "react";
 import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 
+import theme from "./theme";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
