f7c28a89b7a6b2511a7fa2c7744e77a22746646b
Eric Mahoney <eric.mahoney22@gmail.com>
2021-11-03 22:14:56 -0400

    created a theme with built out colors for the project

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      dark: "rgb(34,34,59)",
+      main: "rgb(74, 78, 105)",
+      light: "rgb(154, 140, 152",
+    },
+    secondary: {
+      main: "rgb(201, 173, 167)",
+      light: "rgb(242, 233, 228)",
+    },
+  },
+});
+
+export default theme;
