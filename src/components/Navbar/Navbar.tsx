3a6be7724144fe80e8247426dfbf131a6fe12d38
Eric Mahoney <eric.mahoney22@gmail.com>
2021-11-03 20:24:08 -0400

    created a basic navbar with a little logo

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Typography variant="h6">startpage</Typography>
+    </div>
+  );
+};
+
+export default Navbar;
